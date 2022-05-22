f174f01d66306b88f9ddcc80a0c8fd869939ed24
SukhenduBhukta <sukhendu.bhukta789@gmail.com>
2022-05-22 21:04:05 +0530

    yy

--- all/js/main.js
+++ all/js/main.js
@@ -2,6 +2,6 @@ VanillaTilt.init(document.querySelectorAll(".card"), {
     max: 25,
     speed: 400,
     glare: true,
-    maxGlare: .001,
-    gyroscope: true
+    "maxGlare": .01,
+    gyroscope: true,
 });
