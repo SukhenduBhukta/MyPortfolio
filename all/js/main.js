dead3f7cc250c5063efbfbf5a9d58e4c8014fe19
SukhenduBhukta <sukhendu.bhukta789@gmail.com>
2022-05-28 17:11:48 +0530

    complete responsive

--- all/js/main.js
+++ all/js/main.js
@@ -5,3 +5,16 @@ VanillaTilt.init(document.querySelectorAll(".card"), {
     "maxGlare": .01,
     gyroscope: true,
 });
+
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+})
+
+document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+}))
